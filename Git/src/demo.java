ed3831d4fd5f1fca61cf49f26445da038c0a47eb
LeNgocVanAnh <anhle161108@gmai.com>
2016-11-02 17:17:50 +0700

    Init project

---
+++ Git/src/demo.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author US
+ *
+ */
+public class demo extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+demo m = new demo();
+m.setDefaultCloseOperation(EXIT_ON_CLOSE);
+m.setVisible(true);
+	}
+
+}
